bad607af23fb95e0495e15971435562aeaafe5cd
afrxx09 <afrxx09@student.lnu.se>
2016-01-09 01:49:53 +0100

    moved tests around

--- test/integration/components/asd-nav_test.js
+++ test/integration/components/asd-nav_test.js
@@ -3,6 +3,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import AsdNav from '../../../lib/components/asd-nav';
+import Nav from '../../../lib/components/vanillanav/nav';
+import List from '../../../lib/components/vanillanav/list';
+import ListItem from '../../../lib/components/vanillanav/list-item';
 
 describe('AsdNav', () => {
     
@@ -15,9 +18,33 @@ describe('AsdNav', () => {
             shallowRenderer.render(<AsdNav/>);
             asdNav = shallowRenderer.getRenderOutput();
         });
+        
+        it('has no links', () => {
+            assert.strictEqual(asdNav.props.children.props.links.length, 0, 'should not have links when no routes are supplied.');
+        });
 
-        it('has default id "asd-nav"', () => {
-            assert.strictEqual(asdNav.props.config.id, 'asd-nav', 'does not set default id to "asd-nav".');
+        it('has vanilla nav as default nav component', () => {
+            assert.deepEqual(
+                asdNav.props.config.components.Nav,
+                Nav,
+                'Nav does not defaults to vanilla nav'
+            );
+        });
+        
+        it('has vanilla nav\'s ListItem as default list item component', () => {
+            assert.deepEqual(
+                asdNav.props.config.components.ListItem,
+                ListItem,
+                'ListItem does not defaults to vanilla nav ListItem'
+            );
+        });
+        
+        it('has vanilla nav\'s List as default list component', () => {
+            assert.deepEqual(
+                asdNav.props.config.components.List,
+                List,
+                'List does not defaults to vanilla nav List'
+            );
         });
         
     });
--- test/unit/components/asd-nav_test.js
+++ test/unit/components/asd-nav_test.js
@@ -3,9 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import AsdNav from '../../../src/components/asd-nav';
-import Nav from '../../../src/components/vanillanav/nav';
-import List from '../../../src/components/vanillanav/list';
-import ListItem from '../../../src/components/vanillanav/list-item';
 
 describe('AsdNav', () => {
     
@@ -23,29 +20,10 @@ describe('AsdNav', () => {
             assert.strictEqual(asdNav.props.config.id, 'asd-nav', 'does not set default id to "asd-nav".');
         });
         
-        it('has no links', () => {
-            assert.strictEqual(asdNav.props.children.props.links.length, 0, 'should not have links when no routes are supplied.');
-        });
-        
         it('is root element', () => {
             assert(asdNav.props.children.props.rootNav, 'is not set to root element.');
         });
         
-        it('has vanilla nav as default', () => {
-            assert.deepEqual(
-                asdNav.props.config.components.Nav,
-                Nav,
-                'Nav defaults to vanilla nav'
-            );
-        });
-        
-        it('has vanilla nav\'s ListItem as default', () => {
-            assert.deepEqual(
-                asdNav.props.config.components.ListItem,
-                ListItem,
-                'ListItem defaults to vanilla nav ListItem'
-            );
-        });
         
     });
     
